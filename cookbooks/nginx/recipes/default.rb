12115d5ed5df32f2b12591d377511286d25b11db
hiroki nigorinuma <nigohiroki@gmail.com>
2013-11-28 19:07:41 +0900

    fix

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -1,5 +1,7 @@
-remote_file "/tmp/nginx" do
-  source "http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm"
+bash "install_nginx" do
+  code <<-EOH
+    rpm -ivh http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm
+  EOH
 end
 
 package "nginx" do
