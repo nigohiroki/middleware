aac64f760028fb768dd08f3a470b97bdfacf03a5
hiroki nigorinuma <nigohiroki@gmail.com>
2013-11-28 15:57:36 +0900

    initial commit

---
+++ spec/default/memcached_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe package('memcached') do
+  it { should be_installed }
+end
+
+describe service('memcached') do
+  it { should be_enabled   }
+  it { should be_running   }
+end
+
+describe port(11211) do
+  it { should be_listening }
+end
+
