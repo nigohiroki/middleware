cddc3782be63aed08e743ad63d0e30f09861f674
hiroki nigorinuma <nigohiroki@gmail.com>
2013-11-28 15:59:59 +0900

    cookbooks

---
+++ cookbooks/nginx/recipes/default.rb
@@ -0,0 +1,11 @@
+remote_file "/tmp/nginx" do
+  source "http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm"
+end
+
+package "nginx" do
+  action :install
+end
+
+service "nginx" do
+  action [:enable, :start]
+end
